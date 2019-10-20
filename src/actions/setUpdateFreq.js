56debf585467b917745c440230038f25580c9b00
KayinCheung <cheungkayin00@gmail.com>
2019-10-20 12:13:30 +0800

    SetFrequency saves server response value, instead of user input

--- src/actions/setUpdateFreq.js
+++ src/actions/setUpdateFreq.js
@@ -1,28 +1,21 @@
-import { SET_UPDATE_FREQ } from './types'
+import { SET_UPDATE_FREQ } from "./types";
 
+export const setUpdateFreq = freq => dispatch => {
+  const data = { update_frequency: freq };
 
-export const setUpdateFreq = (freq) => dispatch => {
-
-    const data = { "update_frequency": freq };
-
-    fetch(`http://127.0.0.1:5000/updatefreq`, {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            dispatch({
-                type: SET_UPDATE_FREQ,
-                freq: freq,
-            })
-
-
-        }
-        )
-
-
-}
+  fetch(`http://127.0.0.1:5000/updatefreq`, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      dispatch({
+        type: SET_UPDATE_FREQ,
+        freq: data.freq
+      });
+    });
+};
